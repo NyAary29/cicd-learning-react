2fe4e7a540fc7d45ad7c03c1fca2639fd72bad96
Manana29 <dev.07@crea-tic.net>
2026-04-24 16:31:58 +0300

    add ci/cd workfow

---
+++ my-react-app/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/cicd-learning-react/',
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
